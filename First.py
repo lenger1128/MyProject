06a2fe37df90b99edcb313c0f90299e995853adc
lenger1128 <philippe.wang@foxmail.com>
2019-07-12 00:04:59 +0800

    update .gitignore

--- First.py
+++ First.py
@@ -1,1 +1,9 @@
-Test testset123456
+class MyClass:
+    def __init__(self):
+        self.name='WangYu'
+    def speak(self):
+        print ("My name is {}".format(self.name))
+
+if(__name__=='__main__'):
+    A=MyClass()
+    A.speak()
